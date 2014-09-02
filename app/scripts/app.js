07b17e2d876e3dc7a2f6c5b23454fa6cb218f8aa
XSR71 <xsr71@yahoo.com>
2014-09-01 21:01:48 -0300

    Added local storage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,30 @@
  * Main module of the application.
  */
 angular
-  .module('simpleTodoApp', [
+    .module('simpleTodoApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'LocalStorageModule',
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(['localStorageServiceProvider',
+        function (localStorageServiceProvider) {
+            localStorageServiceProvider.setPrefix('ls');
+        }])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,23 @@
  * Controller of the simpleTodoApp
  */
 angular.module('simpleTodoApp')
-  .controller('MainCtrl', function ($log, $scope) {
-	  $scope.todos = [];
-	  $scope.todo = '';
-	  $scope.addTodo = function () {
-		  if ($scope.todo) {
-			  $log.log('Adding todo: ' + $scope.todo);
-			  $scope.todos.push($scope.todo);
-			  $scope.todo = '';
-		  }
-	  };
-	  $scope.removeTodo = function (index) {
-		  $scope.todos.splice(index, 1);
-	  };
-  });
+    .controller('MainCtrl', function ($log, $scope, localStorageService) {
+        var todosInStore = localStorageService.get('todos');
+
+        $scope.todos = todosInStore && todosInStore.split('\n') || [];
+        $scope.$watch('todos', function () {
+            localStorageService.add('todos', $scope.todos.join('\n'));
+        }, true);
+
+        $scope.todo = '';
+        $scope.addTodo = function () {
+            if ($scope.todo) {
+                $log.log('Adding todo: ' + $scope.todo);
+                $scope.todos.push($scope.todo);
+                $scope.todo = '';
+            }
+        };
+        $scope.removeTodo = function (index) {
+            $scope.todos.splice(index, 1);
+        };
+    });
